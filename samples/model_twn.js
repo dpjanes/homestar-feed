f53511e785db4f1904bab944cb87999c9c6f34aa
David Janes <davidjanes@davidjanes.com>
2015-02-12 09:06:51 -0500

    iotdb -> homestar + cleanup samples

--- samples/model_twn.js
+++ samples/model_twn.js
@@ -3,22 +3,23 @@
  *
  */
 
-var iotdb = require("iotdb");
+var homestar = require("homestar");
+var _ = homestar._;
 
 var ModelBinding = require('../TWNCurrentWeather');
 var feed_iri = "http://rss.theweathernetwork.com/weather/caon0696";
 
-wrapper = iotdb.bridge_wrapper(ModelBinding.binding, { iri: feed_iri });
+wrapper = _.bridge_wrapper(ModelBinding.binding, { iri: feed_iri });
 wrapper.on('model', function(model) {
     model.on_change(function(model) {
         console.log("+ state\n ", model.thing_id(), model.state());
     });
     model.on_meta(function(model) {
-        console.log("+ meta\n ", model.thing_id(), model.meta().state());
+        console.log("+ meta\n ", model.thing_id(), .ld.compact(model.meta().state()));
     });
     
-    console.log("+ discovered\n ", model.meta().state(), "\n ", model.thing_id());
+    console.log("+ discovered\n ", .ld.compact(model.meta().state()), "\n ", model.thing_id());
 })
 wrapper.on('ignored', function(bridge) {
-    console.log("+ ignored\n ", bridge.meta());
+    console.log("+ ignored\n ", .ld.compact(bridge.meta()));
 });
