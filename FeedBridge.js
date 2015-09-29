1f2006c07a5c810e3711e08c95ce2b0a202cc7b4
David Janes <davidjanes@davidjanes.com>
2015-09-29 04:59:09 -0400

    removed debugging

--- FeedBridge.js
+++ FeedBridge.js
@@ -287,7 +287,6 @@ FeedBridge.prototype._process = function (body) {
                     cookd: {},
                 };
                 self.connectd.data_in(paramd);
-                console.log("HERE:XXX", paramd.cookd);
                 self.pulled(paramd.cookd);
             } else {
                 self.pulled(self._flatten(item));
