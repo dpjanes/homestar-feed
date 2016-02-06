c2c229634fde381c92fb1ee2d79ff4fc9817f811
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:26:57 -0500

    fixed up 'push' validation

--- FeedBridge.js
+++ FeedBridge.js
@@ -158,7 +158,7 @@ FeedBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
